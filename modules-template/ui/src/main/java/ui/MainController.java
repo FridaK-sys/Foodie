a9eaff56921442419d343b31b6278c20b66cb68b
Nicoline Mork <nicolimo@stud.ntnu>
2021-09-15 15:12:31 +0200

    added listView

--- modules-template/ui/src/main/java/ui/MainController.java
+++ modules-template/ui/src/main/java/ui/MainController.java
@@ -1,6 +1,13 @@
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import core.Recipe;
+
 public class MainController {
     
+
+    @FXML
+    private ListView<Recipe> mainListView;
     
 }
