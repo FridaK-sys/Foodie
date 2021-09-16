0da68c4e67a51fce07574abb4a01e2120791230a
Nicoline Mork <nicolimo@stud.ntnu>
2021-09-16 13:09:29 +0200

    basic working controller implementation

--- modules-template/ui/src/main/java/ui/CookbookApp.java
+++ modules-template/ui/src/main/java/ui/CookbookApp.java
@@ -1,29 +1,26 @@
 package ui;
  
+import java.net.URL;
 import java.util.List;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class CookbookApp extends Application{
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        launch(args);
+        launch(CookbookApp.class, args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("Main.fxml"));
+        // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("Main.fxml"));
+        URL fxmlLocation = getClass().getResource("Main.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
 
--- modules-template/ui/src/main/java/ui/MainController.java
+++ modules-template/ui/src/main/java/ui/MainController.java
@@ -1,13 +1,44 @@
 package ui;
 
+import javafx.collections.FXCollections;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import core.Recipe;
+import core.Cookbook;
+import core.Ingredient;
+import javafx.fxml.Initializable;
 
-public class MainController {
+
+public class MainController implements Initializable{
+
+    private Cookbook mainBook = new Cookbook();
+    private ObservableList<Recipe> recipes = FXCollections.observableArrayList();
     
+
 
     @FXML
     private ListView<Recipe> mainListView;
+
+
+    
+    public void initialize(URL url, ResourceBundle rb) {
+
+        Recipe recipe1 = new Recipe("recipe1", 2);
+		recipe1.addIngredient(new Ingredient("Fisk", 3, "dl"));
+
+		Recipe recipe2 = new Recipe("recipe2", 2);
+        recipe2.addIngredient(new Ingredient("Fisk", 3, "dl"));
+
+        mainBook.addRecipe(recipe1);
+        mainBook.addRecipe(recipe2);
+        // mainBook.addRecipe(recipe2);
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+        mainListView.getItems().addAll(recipes);
+        // recipes.add(("e"));
+    }
     
 }
