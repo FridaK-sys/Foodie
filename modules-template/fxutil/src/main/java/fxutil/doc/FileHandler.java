9a45bd9e22e681b0f6a88a4f2ebb135290295db7
Juni Sæther Skarpaas <juniss@stud.ntnu.no>
2021-09-21 08:59:48 +0000

    Laget filbehandling

--- modules-template/fxutil/src/main/java/fxutil/doc/FileHandler.java
+++ modules-template/fxutil/src/main/java/fxutil/doc/FileHandler.java
@@ -13,7 +13,7 @@ public class FileHandler {
 				newLine += "/n" + recipe.getDescription();
 	
 				if (index < cookbook.getRecipes().size()) {
-					newLine += "\n" + "#"; //sjekk om funker #
+					newLine += "\n";
 
 				}
 				fileWriter.write(newLine);
@@ -26,55 +26,30 @@ public class FileHandler {
     }
 
     @Override
-	public void readRecipeFromFile(String filename, Cookbook cookbook) throws FileNotFoundException {
+	public void readRecipesFromFile(String filename, Cookbook cookbook) throws FileNotFoundException {
 		try (Scanner scanner = new Scanner(new File(filename))) {
 			while (scanner.hasNextLine()) {
-				String recipe = scanner.nextLine();
-				String[] checkSteps = recipe.split("#");
-
-				String[] recipeElements = checkSteps[0].split("/n"); //før ; 
-                String[] recipeInformation = checkSteps[0].split(";");
+				String[] recipeInformation = scanner.nextLine().split(";");
 				String recipeTitle = recipeInformation[0];
 				String portions = recipeInformation[1];
 				int intPortions = Integer.parseInt(portions);
 				Recipe newRecipe = new Recipe(recipeTitle, intPortions);
-			
-				newRecipe.setDescription(recipeElements[2]);
-				
-				String[] ingredients = recipeElements[1].split("&");
+
+				String[] ingredients = scanner.NextLine().split("&");
 				for (String ing : ingredients) {
 					String[] ingredientElements = ing.split(";");
 					
 					Ingredient newIngredient = new Ingredient(ingredientElements[0],
 					    Double.parseDouble(ingredientElements[1]), ingredientElements[2]);
 					newRecipe.addIngredient(newIngredient);
-					
-
-				}
-				if (checkSteps.length > 2) {
-					String[] steps = checkSteps[1].split("@");
-					List<String> stepsAsList = Arrays.asList(steps);
-					newRecipe.setSteps(stepsAsList);
-
-					String[] tags = (checkSteps[2].split("[\\[\\]]")[1]).split(", ");
-					int[] binaryTags = new int[5];
-					for (int i = 0; i < 5; i++) {
-						binaryTags[i] = Integer.parseInt(tags[i]);
-					}
-					newRecipe.setTags(binaryTags);
-				} else {
-					String[] tags = (checkSteps[1].split("[\\[\\]]")[1]).split(", ");
-					int[] binaryTags = new int[5];
-					for (int i = 0; i < 5; i++) {
-						binaryTags[i] = Integer.parseInt(tags[i]);
-					}
-					newRecipe.setTags(binaryTags);
 				}
 
+				String description = scanner.NextLine();
+				newRecipe.setDescription(description);
+				
 				cookbook.addRecipe(newRecipe);
-
 			}
-//			scanner.close();
+			scanner.close();
 		}
 	}
     
