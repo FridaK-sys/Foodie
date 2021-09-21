1287775a8a508d96bc598ed73e7c75af2ccc6bb5
isabeg <isabeg@stud.ntnu.no>
2021-09-21 14:10:35 +0200

    Fixed methods in RecipeTest

--- modules-template/core/src/test/java/core/RecipeTest.java
+++ modules-template/core/src/test/java/core/RecipeTest.java
@@ -2,30 +2,37 @@ package core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class RecipeTest {
 
 	private Recipe recipe;
     private Ingredient ingredient1, ingredient2, ingredient3;
-    private List<Ingredient> ingredients = new ArrayList<>(ingredient1, ingredient2, ingredient3);
+    private List<Ingredient> ingredients = new ArrayList<>();
 	
 	@BeforeEach
 	public void setUp() {
         ingredient1 = new Ingredient("Mel", 200, "g");
         ingredient2 = new Ingredient("Egg", 2, "stk");
         ingredient3 = new Ingredient("Sukker", 1.5, "dl");
+		ingredients.add(ingredient1);
+		ingredients.add(ingredient2);
 		recipe = new Recipe("Bløtkake", "Den beste oppskriften på bløtkake!", 1, ingredients);
 	}
 	
 	@Test
 	public void testConstructor() {
-		assertEquals(ingredient.getName(), "Mel");
-        assertEquals(ingredient.getAmount(), 200);
-        assertEquals(ingredient.getUnit(), "g");
-		
+		assertEquals(recipe.getName(), "Bløtkake");
+        assertEquals(recipe.getDescription(), "Den beste oppskriften på bløtkake!");
+		assertEquals(recipe.getPortions(), 1);
+		assertEquals(recipe.getIngredients(), ingredients);
+
 		assertThrows(IllegalArgumentException.class, () -> {
 			new Ingredient("%:)", 2, "dl");
 		}, "Invalid name for ingredient");
@@ -46,32 +53,62 @@ public class RecipeTest {
 	
 	@Test
 	public void testSetName() {
-        ingredient.setName("Smør");
-		assertEquals(ingredient.getName(), "Smør");
+        recipe.setName("Muffins");
+		assertEquals(recipe.getName(), "Muffins");
 		
 		assertThrows(IllegalArgumentException.class, () -> {
-			ingredient.setName("%:)");
-		}, "Invalid name for ingredient");
+			recipe.setName("%:)");
+		}, "Invalid name for recipe");
         
-		 assertThrows(IllegalArgumentException.class, () -> {
-			new Ingredient("ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ", 2, "dl");
-		}, "Name of ingredient contains to many characters");
+		assertThrows(IllegalArgumentException.class, () -> {
+			recipe.setName("ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ");
+		}, "Name of recipe contains to many characters");
     }
 
 
 	@Test
-	public void testSetAmount() {
-        ingredient.setAmount(250);
-		assertEquals(ingredient.getAmount(), 250);
+	public void testSetDescription() {
+        recipe.setDescription("My favorite recipe");
+		assertEquals(recipe.getDescription(), "My favorite recipe");
+    }
+
+
+	@Test
+	public void testSetPortions() {
+        recipe.setPortions(2);
+		assertEquals(recipe.getPortions(), 2);
 		
 		assertThrows(IllegalArgumentException.class, () -> {
-			ingredient.setAmount(0);
-		}, "Amount cannot be zero");
+			recipe.setPortions(0);
+		}, "Portions cannot be zero");
 
 		assertThrows(IllegalArgumentException.class, () -> {
-			ingredient.setAmount(-2);
-		}, "Amount cannot be negative");
-        
-    }
+			recipe.setPortions(-1);
+		}, "Portions cannot be negative");
+	}
+
+		@Test
+		public void testAddIngredient() {
+        	recipe.addIngredient(ingredient3);
+			assertEquals(recipe.getIngredients(), Arrays.asList(ingredient1, ingredient2, ingredient3));
+		}
+
+		@Test
+		public void testRemoveIngredient() {
+        	recipe.removeIngredient(ingredient2);
+			assertEquals(recipe.getIngredients(), Arrays.asList(ingredient1, ingredient3));
+
+    	}
+
+
+		@Test
+		public void testRemoveIngredientString() {
+			recipe.removeIngredient("Mel");
+			assertEquals(recipe.getIngredients(), Arrays.asList(ingredient2, ingredient3));
+			
+			assertThrows(IllegalArgumentException.class, () -> {
+				recipe.removeIngredient("Bakepulver");
+			}, "Recipe does not contain this ingredient");
+    	}
 
 }
