d894ecc45284182822351c88736d14063f9506be
Nicoline Mork <nicolimo@stud.ntnu>
2021-09-23 08:03:09 +0200

    tilbake-funksjon

--- modules-template/ui/src/main/java/ui/ViewRecipeController.java
+++ modules-template/ui/src/main/java/ui/ViewRecipeController.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,10 +10,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 public class ViewRecipeController implements Initializable {
 
@@ -75,5 +81,17 @@ public class ViewRecipeController implements Initializable {
         }
         
     }
+
+    public void changeSceneToMain(ActionEvent ea) throws IOException {
+        URL fxmlLocation = getClass().getResource("Main.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+        
+        Parent root = fxmlLoader.load();
+        Scene viewRecipesScene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) ea.getSource()).getScene().getWindow();
+        stage.setScene(viewRecipesScene);
+        stage.show();
+}
 
 }
