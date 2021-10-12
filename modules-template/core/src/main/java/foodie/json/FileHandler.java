a3a695a83fb95e76b0b5ccaf3f6e4236a171e7b1
Frida Rapp Karlsen <fridark@stud.ntnu.no>
2021-10-12 11:59:08 +0200

    Legger til kommentarer til readRecipesFromFile

--- modules-template/core/src/main/java/foodie/json/FileHandler.java
+++ modules-template/core/src/main/java/foodie/json/FileHandler.java
@@ -8,8 +8,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.FilterOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.Writer;
 import java.util.Iterator;
 
@@ -26,102 +28,76 @@ import foodie.core.Recipe;
 
 public class FileHandler {
 
-	
+  // gjorde en endring siden scanner.nextLine ikke deler på /n
+  // endret til å dele opp navn, porsjoner, ingredienser og beskrivelse på ;
+  // Ingredienser deles opp med & og de tre delene de består av med :
+  public void writeRecipesToFile(String filename, Cookbook cookbook) throws IOException {
+    // @SuppressWarnings("unchecked")
+    JSONArray recipes = new JSONArray();
+    JSONObject mainObj = new JSONObject();
 
-    // gjorde en endring siden scanner.nextLine ikke deler på /n
-	// endret til å dele opp navn, porsjoner, ingredienser og beskrivelse på ;
-	//Ingredienser deles opp med & og de tre delene de består av med :
-    public void writeRecipesToFile(String filename, Cookbook cookbook) throws IOException	{ 
-	//@SuppressWarnings("unchecked")
-	    JSONArray recipes = new JSONArray();
-		JSONObject mainObj = new JSONObject();
+    for (Recipe recipe : cookbook.getRecipes()) {
+      for (Ingredient ingredient : recipe.getIngredients()) {
+        JSONObject ing = new JSONObject();
+        ing.put("Name", ingredient.getName());
+        ing.put("Amount", ingredient.getAmount());
+        ing.put("Unit", ingredient.getUnit());
 
-		for(Recipe recipe : cookbook.getRecipes()){
-			for(Ingredient ingredient : recipe.getIngredients()) {
-				JSONObject ing = new JSONObject();
-				ing.put("Name", ingredient.getName());
-				ing.put("Amount", ingredient.getAmount());
-				ing.put("Unit", ingredient.getUnit());
+        JSONObject rec = new JSONObject();
+        rec.put("Name", recipe.getName());
+        rec.put("Portions", recipe.getPortions());
+        rec.put("Ingrediens", ing);
 
-				JSONObject rec = new JSONObject();
-				rec.put("Name", recipe.getName());
-				rec.put("Portions", recipe.getPortions());
-				rec.put("Ingrediens", ing);
+        recipes.add(rec);
+        mainObj.put("Recipes", recipes);
 
-				recipes.add(rec);
-				mainObj.put("Recipes", recipes);
-				
-			}
-		}
-  		
-        //Write JSON file
-        try (Writer file = new OutputStreamWriter(new FileOutputStream(filename), "UTF-8")) { 
-            file.write(mainObj.toJSONString());  
-            file.flush();
- 
-        } catch (IOException e) {
-            e.printStackTrace();
-    	}
-	}
-
-	public void writeRecipeToFile(String filename, Recipe recipe) {
-		JSONObject res = new JSONObject();
-		for(Ingredient ingredient : recipe.getIngredients()) {
-			JSONObject ing = new JSONObject();
-			ing.put("Name", ingredient.getName());
-			ing.put("Amount", ingredient.getAmount());
-			ing.put("Unit", ingredient.getUnit());
-
-			res.put("Name", recipe.getName());
-			res.put("Portions", recipe.getPortions());
-			res.put("Ingrediens", ing);
-
-			
-		}
-		//Write JSON file
-        try (Writer file = new OutputStreamWriter(new FileOutputStream(filename), "UTF-8")) { 
-            file.write(res.toJSONString());  
-            file.flush();
- 
-        } catch (IOException e) {
-            e.printStackTrace();
-    	}
-
-	}
-
-	public void readRecipesFromFile(String filename, Cookbook cookbook) throws FileNotFoundException {
-		//JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
-         
-        try (InputStreamReader input = new InputStreamReader(new FileInputStream(filename))) 
-        { //try (FileReader reader = new FileReader(filename))
-            //Read JSON file
-            Object obj = jsonParser.parse(new InputStreamReader(new FileInputStream(filename))); //jsonParser.parse(new FileReader(filename));
-			JSONObject Jobj = (JSONObject) obj;
-            JSONArray recipeList = (JSONArray) Jobj.get("Recipes");
-
-			Iterator<JSONObject> iterator = recipeList.iterator();
-			while(iterator.hasNext()) {
-				System.out.println(iterator.next());
-			}
-
-        } 
-         catch (Exception e) {
-            e.printStackTrace();
-        } 
+      }
     }
 
-    
+    // Write JSON file
+    try (Writer file = new OutputStreamWriter(new FileOutputStream(filename), "UTF-8")) {
+      file.write(mainObj.toJSONString());
+      file.flush();
 
-  public void readRecipeFromFile(String filename, Cookbook cookbook) throws FileNotFoundException {
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void writeRecipeToFile(String filename, Recipe recipe) {
+    JSONObject res = new JSONObject();
+    for (Ingredient ingredient : recipe.getIngredients()) {
+      JSONObject ing = new JSONObject();
+      ing.put("Name", ingredient.getName());
+      ing.put("Amount", ingredient.getAmount());
+      ing.put("Unit", ingredient.getUnit());
+
+      res.put("Name", recipe.getName());
+      res.put("Portions", recipe.getPortions());
+      res.put("Ingrediens", ing);
+
+    }
+    // Write JSON file
+    try (Writer file = new OutputStreamWriter(new FileOutputStream(filename), "UTF-8")) {
+      file.write(res.toJSONString());
+      file.flush();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public void readRecipesFromFile(String filename, Cookbook cookbook) throws FileNotFoundException {
     // JSON parser object to parse read file
     JSONParser jsonParser = new JSONParser();
 
-    try (FileReader reader = new FileReader(filename)) {
-      // Read JSON file
-      Object obj = jsonParser.parse(new FileReader(filename));
+    try (InputStream input = new FileInputStream(filename)) { // try (FileReader reader =
+                                                              // new FileReader(filename))
+      Reader filereader = new InputStreamReader(input, "UTF-8"); // Read JSON file
+      Object obj = jsonParser.parse(filereader); // jsonParser.parse(new
+                                                 // FileReader(filename));
       JSONObject Jobj = (JSONObject) obj;
-
       JSONArray recipeList = (JSONArray) Jobj.get("Recipes");
 
       Iterator<JSONObject> iterator = recipeList.iterator();
