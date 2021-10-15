0958ea4b7cfc72f58e867e08fee66c10a49fdc68
Nicoline Mork <nicolimo@stud.ntnu>
2021-10-15 13:37:17 +0200

    Setter opp appen med gitte oppskrifter til testing

--- modules-template/ui/src/test/java/ui/AbstractAppTest.java
+++ modules-template/ui/src/test/java/ui/AbstractAppTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import core.Cookbook;
 import core.Ingredient;
 import core.Recipe;
 import javafx.scene.control.ListView;
@@ -35,10 +36,29 @@ public abstract class AbstractAppTest extends ApplicationTest {
     }
 
     public void testRecipes(List<Recipe> re, Recipe... recipes) {
-        for (int j = 0; j < 2; j++) {
+        for (int j = 0; j < re.size() -1; j++) {
             checkRecipe(re.get(j), recipes[j]);
         }
 
+    }
+
+    public Cookbook setUpCookBook(){
+        Recipe recipe1 = new Recipe("Taco", 4);
+        Ingredient ing1 = new Ingredient("Kjøtt", 500, "g");
+        Ingredient ing2 = new Ingredient("Lomper", 3, "stk");
+        recipe1.addIngredient(ing1);
+        recipe1.addIngredient(ing2);
+        recipe1.setDescription("Enkel taco");
+        Recipe recipe2 = new Recipe("Fiskekaker", 3);
+        Ingredient ing3 = new Ingredient("Fisk", 3, "dl");
+        Ingredient ing4 = new Ingredient("Purre", 1, "stk");
+        recipe2.addIngredient(ing3);
+        recipe2.addIngredient(ing4);
+        recipe2.setDescription("Dette er gode fiskekaker");
+        Cookbook referenceBook = new Cookbook();
+        referenceBook.addRecipe(recipe1);
+        referenceBook.addRecipe(recipe2);
+        return referenceBook;
     }
 
 }
