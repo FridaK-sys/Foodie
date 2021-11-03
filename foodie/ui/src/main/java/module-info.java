96cc447f5fc032deadeaa49f098f73181ac8e8b1
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-03 12:34:15 +0100

    fix: required modules updated

--- foodie/ui/src/main/java/module-info.java
+++ foodie/ui/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module calc.ui {
   requires transitive calc.core;
   requires javafx.controls;
+  requires transitive javafx.base;
   requires javafx.fxml;
   requires json.simple;
+
   exports ui;
 
   opens ui to javafx.graphics, javafx.fxml;
