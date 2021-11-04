93d191a2845f47cd8f3d73a481043ce55dbbad5d
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-04 11:40:24 +0100

    fix: removed unused import

--- foodie/ui/src/main/java/ui/ViewRecipeController.java
+++ foodie/ui/src/main/java/ui/ViewRecipeController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import core.Cookbook;
 import core.Ingredient;
 import core.Recipe;
 import javafx.collections.FXCollections;
