921bce72986f7eb6546fb0f3f0fdd151f924f434
isabeg <isabeg@stud.ntnu.no>
2021-11-16 13:48:48 +0100

    fix: file management test

--- foodie/core/src/test/java/json/CookbookModuleTest.java
+++ foodie/core/src/test/java/json/CookbookModuleTest.java
@@ -2,14 +2,9 @@ package json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,16 +14,12 @@ import core.Ingredient;
 
 public class CookbookModuleTest {
 
-  // {"lists":[{"name": "todo",
-  // "items":[{"text":"item1","checked":false},{"text":"item2","checked":true}]}]}
-
   private static ObjectMapper mapper;
-  private static Recipe recipe;
-  private static Recipe recipe2;
   private static Ingredient ingredient1, ingredient2, ingredient3, ingredient4;
-  private static List<Ingredient> ingredients = new ArrayList<>();
+  private static List<Ingredient> ingredients1 = new ArrayList<>();
   private static List<Ingredient> ingredients2 = new ArrayList<>();
-  private static List<Recipe> recipeList = new ArrayList<>();
+  private static Recipe recipe1, recipe2;
+  private static List<Recipe> recipes = new ArrayList<>();
   private static Cookbook cookbook;
 
   @BeforeAll
@@ -37,100 +28,107 @@ public class CookbookModuleTest {
 
   }
 
-  final static String newCookbook = """
-      {
-        "Recipes":
-        [{
-            "Favorite":false,
-            "Portions":1,
-            "Description":"Den beste oppskriften på bløtkake!",
-            "Ingredients":
-            [{
-              "Amount":200.0,
-              "Unit":"g",
-              "Name":"Mel"
-            },
-            {"Amount":2.0,
-            "Unit":"stk",
-            "Name":"Egg"
-          }],
-          "Label":"Breakfast",
-          "Name":"Bløtkake"
-        },{
-          "Favorite":true,
-          "Portions":1,
-          "Description":"Varm og god dessert!",
-          "Ingredients":
-          [{
-            "Amount":1.5,
-            "Unit":"dl",
-            "Name":"Sukker"
-          },{
-            "Amount":1.0,
-            "Unit":"dl",
-            "Name":"Kakao"
-          }],
-          "Label":"",
-          "Name":"Kakao"
-        }],
-        "Name":"Kokebok"}
-      }
-      """;
-
   static Cookbook createCookbook() {
-    ingredient1 = new Ingredient("Mel", 200, "g");
-    ingredient2 = new Ingredient("Egg", 2, "stk");
-    ingredient3 = new Ingredient("Sukker", 1.5, "dl");
-    ingredient4 = new Ingredient("Kakao", 1, "dl");
-    ingredients.add(ingredient1);
-    ingredients.add(ingredient2);
+    ingredient1 = new Ingredient("Egg", 2, "stk");
+    ingredient2 = new Ingredient("Kakao", 1, "dl");
+    ingredient3 = new Ingredient("Mel", 200, "g");
+    ingredient4 = new Ingredient("Sukker", 1.5, "dl");
+    ingredients1.add(ingredient1);
+    ingredients1.add(ingredient2);
     ingredients2.add(ingredient3);
     ingredients2.add(ingredient4);
-    recipe = new Recipe("Bløtkake", "Den beste oppskriften på bløtkake!", 1, ingredients);
-    recipe.setLabel("Breakfast");
-    recipe2 = new Recipe("Kakao", "Varm og god dessert!", 1, ingredients2);
-    recipe2.setFav(true);
-    recipeList.add(recipe);
-    recipeList.add(recipe2);
-    cookbook = new Cookbook("Kokebok", recipeList);
+    recipe1 = new Recipe("Brownies", "Den beste oppskriften på brownies!", 1, ingredients1);
+    recipe1.setLabel("Dessert");
+    recipe1.setFav(true);
+    recipe2 = new Recipe("Pepperkaker", "Bestemor sin oppskrift", 4, ingredients2);
+    recipe2.setLabel("Dessert");
+    recipes.add(recipe1);
+    recipes.add(recipe2);
+    cookbook = new Cookbook("Cookbook", recipes);
     return cookbook;
   }
+
+  final static String newCookbook = """
+      {
+        "name": "Cookbook",
+        "recipes" :[
+          {
+            "name": "Brownies",
+            "description": "Den beste oppskriften på brownies!",
+            "portions": 1,
+            "fav": true,
+            "label": "Dessert",
+            "ingredients" :[
+              {
+                "name": "Egg",
+                "amount": 2.0,
+                "unit": "stk"
+              },
+              {
+                "name": "Kakao",
+                "amount": 1.0,
+                "unit": "dl"
+              }
+            ]
+          },
+          {
+            "name": "Pepperkaker",
+            "description": "Bestemor sin oppskrift",
+            "portions": 4,
+            "fav": false,
+            "label": "Dessert",
+            "ingredients" :[
+              {
+                "name": "Mel",
+                "amount": 200.0,
+                "unit": "g"
+              },
+              {
+                "name": "Sukker",
+                "amount": 1.5,
+                "unit": "dl"
+              }
+            ]
+          }
+        ]
+      }
+      """;
 
   @Test
   public void testSerializers() {
     Cookbook cookbook = createCookbook();
     try {
+      for (Recipe recipe : recipes) {
+        recipe.setDescription(recipe.getDescription().replaceAll("\\s+", ""));
+      }
       assertEquals(newCookbook.replaceAll("\\s+", ""), mapper.writeValueAsString(cookbook));
     } catch (JsonProcessingException e) {
       fail(e.getMessage());
     }
   }
 
-  static void checkRecipe(Recipe recipe, String name, String description, int portions, List<Ingredient> ingredients) {
+  static void checkCookbook(Cookbook cookbook, String name) {
+    assertEquals(name, cookbook.getName());
+  }
+
+  static void checkRecipe(Recipe recipe, String name, String description, int portions, boolean fav, String label,
+      List<Ingredient> ingredients) {
     assertEquals(name, recipe.getName());
     assertEquals(description, recipe.getDescription());
     assertEquals(portions, recipe.getPortions());
+    assertEquals(fav, recipe.getFav());
+    assertEquals(label, recipe.getLabel());
     assertEquals(ingredients, recipe.getIngredients());
-
   }
 
   @Test
   public void testDeserializers() {
     try {
-      Cookbook cookbook = mapper.readValue(newCookbook, Cookbook.class);
-      List<Recipe> recipes = new ArrayList<>(cookbook.getRecipes());
-
-      for (Recipe res : recipes) {
-        checkRecipe(res, "Bløtkake", "Den beste oppskriften på bløtkake!", 1, ingredients);
-        checkRecipe(res, "Kakao", "Varm og god dessert", 1, ingredients2);
-
-      }
-      /*
-       * 
-       * assertEquals(TodoItemsSortOrder.CHECKED_UNCHECKED,
-       * model.getSettings().getTodoItemsSortOrder()); } catch
-       * (JsonProcessingException e) { fail(e.getMessage()); }
-       */
+      createCookbook();
+      Cookbook newCookbook2 = mapper.readValue(newCookbook, Cookbook.class);
+      checkCookbook(newCookbook2, "Cookbook");
+      checkRecipe(recipe1, "Brownies", "Den beste oppskriften på brownies!", 1, true, "Dessert", ingredients1);
+      checkRecipe(recipe2, "Pepperkaker", "Bestemor sin oppskrift", 4, false, "Dessert", ingredients2);
 
     } catch (JsonProcessingException e) {
       fail(e.getMessage());
@@ -140,41 +138,21 @@ public class CookbookModuleTest {
 
   @Test
   public void testSerializersDeserializers() {
-    List<Ingredient> ings = new ArrayList<>();
+    Ingredient ingredient1 = new Ingredient("Potet", 3, "stk");
+    List<Ingredient> ingredients = new ArrayList<>();
+    ingredients.add(ingredient1);
+    Recipe recipe = new Recipe("Stekte poteter", "Kutt potetene og stek de", 2, ingredients);
     List<Recipe> recipes = new ArrayList<>();
-
-    Ingredient ing = new Ingredient("Potet", 1, "kg");
-    ings.add(ing);
-    Recipe recipe = new Recipe("Stekte poteter", "Kutt potetene og stek de", 2, ings);
     recipes.add(recipe);
-    ;
-
-    Cookbook cook = new Cookbook("Tilbehør", recipes);
-
+    Cookbook cookbook1 = new Cookbook("Tilbehør", recipes);
     try {
-      String json = mapper.writeValueAsString(cook);
-      Cookbook cookbook2 = mapper.readValue(json, Cookbook.class); // trenger man denne?
-      assertEquals("Stekte poteter", recipe.getName());
-
+      String json = mapper.writeValueAsString(cookbook1);
+      Cookbook cookbook2 = mapper.readValue(json, Cookbook.class);
+      assertEquals(cookbook1.getName(), cookbook2.getName());
+      assertEquals(cookbook1.getRecipes().toString(), cookbook2.getRecipes().toString());
     } catch (JsonProcessingException e) {
       fail(e.getMessage());
     }
   }
-  /*
-   * private final static String defaultSettnings =
-   * "{\"todoItemsSortOrder\":\"UNCHECKED_CHECKED\"}";
-   * 
-   * 
-   * 
-   * @Test public void testTodoSettings() { TodoSettings settings = new
-   * TodoSettings();
-   * settings.setTodoItemsSortOrder(TodoItemsSortOrder.UNCHECKED_CHECKED); try {
-   * String json = mapper.writeValueAsString(settings);
-   * assertEquals(defaultTodoSettings.replaceAll("\\s+", ""),
-   * mapper.writeValueAsString(settings)); TodoSettings settings2 =
-   * mapper.readValue(json, TodoSettings.class);
-   * assertEquals(settings.getTodoItemsSortOrder(),
-   * settings2.getTodoItemsSortOrder()); } catch (JsonProcessingException e) {
-   * fail(e.getMessage()); } }
-   */
+
 }
