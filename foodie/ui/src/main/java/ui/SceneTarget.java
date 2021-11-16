e5011b3672ae7c76115518e05bccdf7aaf8b7fdf
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-16 13:34:58 +0100

    feat: new class to set target for scene change backbutton

---
+++ foodie/ui/src/main/java/ui/SceneTarget.java
@@ -0,0 +1,39 @@
+package ui;
+
+import java.io.IOException;
+import java.lang.ModuleLayer.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.stage.Stage;
+
+
+public class SceneTarget {
+  private Scene scene;
+
+  public SceneTarget(Scene scene) {
+
+  this.scene=scene;
+  }
+
+  /**
+   * Gets an EventHandler that switches to the scene of this SceneTarget.
+   *
+   * @return an EventHandler that switches to the scene of this SceneTarget
+   */
+  public EventHandler<ActionEvent> getActionEventHandler() {
+    return actionEvent -> {
+      Control control = (Control) actionEvent.getSource();
+      Stage stage = (Stage) control.getScene().getWindow();
+      FXMLLoader loader = (FXMLLoader) scene.getUserData();
+
+      IFoodieController controller = loader.getController();
+      controller.update();
+      stage.setScene(scene);
+    };
+  }
+
+}
