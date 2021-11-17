a406bfed615025586220506fa9d843a7488afc42
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-17 10:42:29 +0100

    feat: implements listview

--- foodie/ui/src/main/java/ui/LocalAppController.java
+++ foodie/ui/src/main/java/ui/LocalAppController.java
@@ -1,7 +1,11 @@
 package ui;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 import ui.utils.LocalCookbookAccess;
 
 public class LocalAppController extends AbstractController {
@@ -9,6 +13,26 @@ public class LocalAppController extends AbstractController {
   @Override
   protected void setUpStorage() {
     dataAccess = new LocalCookbookAccess("checkCookbookff.json");
+  }
+
+  @FXML
+  private Pane mainListView;
+
+  @FXML
+  ListViewController mainListViewController;
+
+  public void initialize(URL url, ResourceBundle rb) {
+    setUpStorage();
+    initializeRecipesView();
+  }
+
+  private void initializeRecipesView() {
+    mainListViewController.setCookbookAccess(dataAccess);
+  }
+
+  @Override
+  public void update() {
+    mainListViewController.update();
   }
 
 }
