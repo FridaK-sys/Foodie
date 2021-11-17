0bb43d23ba6fc75dc7ae87b494aefa575c9c8864
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-17 11:01:48 +0100

    fix: remove unused imports

--- foodie/ui/src/main/java/ui/AbstractController.java
+++ foodie/ui/src/main/java/ui/AbstractController.java
@@ -1,34 +1,7 @@
 package ui;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
-
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleButton;
-import core.Cookbook;
-import core.Ingredient;
-import core.Recipe;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+
 import ui.utils.CookbookInterface;
 
 public abstract class AbstractController implements IFoodieController, Initializable {
