f70b4e9c9f30fa9c7c16c335c2c96943c7f4392e
Juni <junitrigger@gmail.com>
2021-11-18 13:18:53 +0100

    style: checkstyle-edits based on warnings

--- foodie/restapi/src/main/java/foodie/restapi/CookbookApplication.java
+++ foodie/restapi/src/main/java/foodie/restapi/CookbookApplication.java
@@ -1,23 +1,25 @@
 package foodie.restapi;
 
-import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import json.CookbookPersistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
- * CookbookApplication that runs the SpringApplication.
+ * CookbookApplication that runs SpringbootApplication.
  */
 @SpringBootApplication
 public class CookbookApplication {
-	@Bean
-	public Module objectMapperModule() {
-		return CookbookPersistence.createModule();
-	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(CookbookApplication.class, args);
-	}
+  @Bean
+  public SimpleModule objectMapperModule() {
+    return CookbookPersistence.createModule();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(CookbookApplication.class, args);
+  }
+
 
 }
