ef37b590f348226dc729ba5f9f256763a4b15a3b
FridaK-sys <frida.karlsen@hotmail.com>
2021-11-18 15:12:00 +0100

    feat: made test class for Remote

---
+++ foodie/ui/src/test/java/ui/RemoteCookbookAccessTest.java
@@ -0,0 +1,86 @@
+package ui;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import core.Cookbook;
+import core.Recipe;
+import ui.utils.RemoteCookbookAccess;
+
+public class RemoteCookbookAccessTest {
+
+  private WireMockConfiguration config;
+  private WireMockServer wireMockServer;
+  private RemoteCookbookAccess cookbookAccess;
+
+  private static final String GET_COOKBOOK_RESPONSE = """
+      {
+        "name": "Cookbook",
+        "recipes" :[
+          {
+            "name": "Brownies",
+            "description": "Den beste oppskriften på brownies!",
+            "portions": 1,
+            "fav": true,
+            "label": "Dessert",
+            "ingredients" :[
+              {
+                "name": "Egg",
+                "amount": 2.0,
+                "unit": "stk"
+              },
+              {
+                "name": "Kakao",
+                "amount": 1.0,
+                "unit": "dl"
+              }
+            ]
+          }]}""";
+
+  @BeforeEach
+  public void startWireMockServerAndSetup() throws URISyntaxException {
+    config = WireMockConfiguration.wireMockConfig().port(8081);
+    wireMockServer = new WireMockServer(config.portNumber());
+    wireMockServer.start();
+    WireMock.configureFor("localhost", config.portNumber());
+    cookbookAccess = new RemoteCookbookAccess(new URI("http://localhost:" + wireMockServer.port() + "/cookbook"));
+  }
+
+  @Test
+  public void testGetCookbook() {
+    stubFor(get(urlEqualTo("/cookbook")).withHeader("Accept", equalTo("application/json")).willReturn(
+        aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(GET_COOKBOOK_RESPONSE)));
+    List<Recipe> recipes = cookbookAccess.getCookbook().getRecipes();
+    assertEquals(1, recipes.size());
+
+  }
+
+  // @Test
+  // public void testAddRecipe(){
+  // stubFor(get(urlEqualTo("/cookbook")).withHeader("Accept",
+  // equalTo("application/json")).willReturn(
+  // aResponse().withStatus(200).withHeader("Content-Type",
+  // "application/json").withBody(GET_COOKBOOK_RESPONSE)));
+
+  // }
+
+  @AfterEach
+  public void stopWireMockServer() {
+    wireMockServer.stop();
+  }
+
+}
