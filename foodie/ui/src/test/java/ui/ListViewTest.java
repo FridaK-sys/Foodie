915e10f7ab28624d8b9adbc2f6acdbebc2a9f918
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-19 12:11:21 +0100

    feat: update tests add testing for toggle buttons and listview

--- foodie/ui/src/test/java/ui/ListViewTest.java
+++ foodie/ui/src/test/java/ui/ListViewTest.java
@@ -1,17 +1,17 @@
 package ui;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.testfx.framework.junit5.ApplicationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+
 
 import core.Cookbook;
 import core.Ingredient;
@@ -23,53 +23,70 @@ import javafx.stage.Stage;
 import json.CookbookPersistence;
 import ui.utils.LocalCookbookAccess;
 
-public class ListViewTest extends ApplicationTest{
-  @BeforeAll
-  public static void setupHeadless() {
-    CookbookApp.supportHeadless();
-  }
+public class ListViewTest extends AbstractAppTest {
 
   private ListViewController controller;
+  
 
   @Override
   public void start(final Stage stage) throws Exception {
-    final FXMLLoader loader = new FXMLLoader(getClass().getResource("TodoList_test.fxml"));
-    final Parent root = loader.load();
-    this.controller = loader.getController();
-    LocalCookbookAccess dataAccess = new LocalCookbookAccess("test-todomodel.json");
-    this.controller.setCookbookAccess(dataAccess);    
-    stage.setScene(new Scene(root));
+    URL fxmlLocation = getClass().getResource("ListView_test.fxml");
+    FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+    Parent root = fxmlLoader.load();
+    Scene scene = new Scene(root);
+    this.controller = fxmlLoader.getController();
+    stage.setScene(scene);
     stage.show();
+    
   }
 
-  private CookbookPersistence cookbookPersistence = new CookbookPersistence();
-  private Recipe recipe;
-  private Ingredient ing1, ing2, ing3;
+
 
   @BeforeEach
   public void setupItems() {
-    Cookbook cookbook = null;
-    try {
-      cookbook = cookbookPersistence.readCookbook(new InputStreamReader(getClass().getResourceAsStream("test-cokbook.json")));
-    } catch (IOException e) {
-      fail("Couldn't load test-cookbook.json");
-    }
-    assertNotNull(cookbook);
+    // Cookbook cookbook = null;
+    setTestData();
     this.controller.setRecipes(cookbook);
-    assertNotNull(cookbook.getRecipes().get(0));
-    this.recipe = cookbook.getRecipes().get(0);
-    assertEquals(3, recipe.getIngredients().size());
-    ing1 = recipe.getIngredients().get(0);
-    ing2 = recipe.getIngredients().get(1);
-    ing3 = recipe.getIngredients().get(2);
+    assertNotNull(cookbook);
+
+    
   }
 
   @Test
-  public void testController_todoList() {
+  public void testController() {
     assertNotNull(this.controller);
-    assertNotNull(this.recipe);
-    // initial todo items
-    testRecipeIngredients(item1, item2, item3);
+    testRecipes(cookbook.getRecipes(), recipe1, recipe2, recipe3);
   }
-  
+
+  @Test
+  public void testRecipeListView() {
+    checkRecipesListViewItems(recipe1, recipe2, recipe3);
+  }
+
+  @Test
+  public void testToggleButtons() {
+    clickOn("#Breakfast");
+    checkRecipesListViewItems(recipe2);
+    clickOn("#All");
+    checkRecipesListViewItems(recipe1, recipe2, recipe3);
+    clickOn("#Fav");
+    checkRecipesListViewItems(recipe3);
+    clickOn("#Breakfast");
+    checkRecipesListViewItems();
+    clickOn("#Fav");
+    clickOn("#Lunch");
+    checkRecipesListViewItems();
+    clickOn("#Dinner");
+    checkRecipesListViewItems(recipe3);
+    clickOn("#All");
+    checkRecipesListViewItems(recipe1, recipe2, recipe3);
+  }
+
+
+
+  @Test
+  public void testChangeSceneToNewRecipeButtonPushed(){
+    clickOn("#newButton");
+  }
+
 }
