14a70445b675a897d66670fc95ccd158a1a54124
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-20 07:30:32 +0100

    feat: bug fixed: added code from being commented, now it works

--- foodie/ui/src/test/java/ui/ViewRecipeControllerTest.java
+++ foodie/ui/src/test/java/ui/ViewRecipeControllerTest.java
@@ -6,71 +6,76 @@ import java.net.URL;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import core.Ingredient;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import ui.ViewRecipeController;
 
 public class ViewRecipeControllerTest extends AbstractAppTest {
 
-    // private ViewRecipeController controller = new ViewRecipeController();
+    private ViewRecipeController controller = new ViewRecipeController();
 
-    // @FXML
-    // private Label recipeTitle;
+    @FXML
+    private Label recipeTitle;
 
-    // @FXML
-    // private Label portions, labelTag;
+    @FXML
+    private Label portions, labelTag;
 
-    // @FXML
-    // private TextArea textField;
+    @FXML
+    private TextArea textField;
 
-    // @Override
-    // public void start(final Stage stage) throws Exception {
+    @Override
+    public void start(final Stage stage) throws Exception {
 
-    // URL fxmlLocation = getClass().getResource("ViewRecipes_test.fxml");
-    // FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
-    // Parent root = fxmlLoader.load();
-    // setTestData();
+        URL fxmlLocation = getClass().getResource("ViewRecipes_test.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+        Parent root = fxmlLoader.load();
+        setTestData();
 
-    // Scene scene = new Scene(root);
-    // this.controller = fxmlLoader.getController();
-    // controller.initData(recipe4, 1, null, dataAccess);
+        Scene scene = new Scene(root);
+        this.controller = fxmlLoader.getController();
+        controller.initData(recipe4, 1, null, dataAccess);
 
-    // stage.setScene(scene);
-    // stage.show();
-    // }
+        stage.setScene(scene);
+        stage.show();
+    }
 
     // @BeforeEach
     // public void setupItems() {
 
     // }
 
-    // @Test
-    // public void testRecipeView() {
-    // assertEquals(recipe4.getName(), recipeTitle.getText());
-    // assertEquals(recipe4.getPortions(), Integer.parseInt(portions.getText()));
-    // for (int i = 0; i < ingredients.size(); i++) {
-    // checkIngredient(recipe4.getIngredients().get(i), ingredients.get(i));
-    // }
-    // }
+    @Test
+    public void testRecipeView() {
+        Label title = lookup("#recipeTitle").query();
+        Label portion = lookup("#portions").query();
+        ListView<Ingredient> ingredientListView = lookup("#ingredientsListView").query();
+        assertEquals(recipe4.getName(), title.getText());
+        assertEquals(recipe4.getPortions(), Integer.parseInt(portion.getText()));
+        for (int i = 0; i < ingredients.size(); i++) {
+            checkIngredient(ingredientListView.getItems().get(i), ingredients.get(i));
+        }
+    }
 
-    // @Test
-    // public void testLabel() {
-    // assertEquals(recipe4.getLabel(), labelTag.getText(),
-    // "Incorrect label, expected: " + recipe4.getLabel() + ", " +
-    // labelTag.getText() + " was displayed");
-    // }
+    @Test
+    public void testLabel() {
+        Label label = lookup("#labelTag").query();
+        assertEquals(recipe4.getLabel(), label.getText(),
+                "Incorrect label, expected: " + recipe4.getLabel() + ", " + label.getText() + " was displayed");
+    }
 
-    // @Test
-    // public void testDescription() {
-    // assertEquals(recipe4.getDescription(), textField.getText(), "Incorrect
-    // description, expected: "
-    // + recipe4.getDescription() + ", " + textField.getText() + " was displayed");
-    // }
+    @Test
+    public void testDescription() {
+        TextArea description = lookup("#textField").query();
+        assertEquals(recipe4.getDescription(), description.getText(), "Incorrect description, expected: "
+                + recipe4.getDescription() + ", " + description.getText() + " was displayed");
+    }
 
 }
