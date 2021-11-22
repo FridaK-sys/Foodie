cfd188f9c9be8c7b993edf3691c5a172ad041d28
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-22 18:06:06 +0100

    test: move test scenechange to application test

--- foodie/ui/src/test/java/ui/AppTest.java
+++ foodie/ui/src/test/java/ui/AppTest.java
@@ -3,14 +3,20 @@ package ui;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
-
+import org.testfx.util.WaitForAsyncUtils;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ui.utils.LocalCookbookAccess;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import java.util.function.Predicate;
+import core.Recipe;
 
 public class AppTest extends AbstractAppTest {
 
@@ -31,6 +37,48 @@ public class AppTest extends AbstractAppTest {
     public void testController() {
         assertNotNull(this.controller, "Error loading controller");
         assertNotNull(this.controller.getCookbook(), "Error accessing data");
+    }
+
+    @Test
+    public void testRecipeListView() {
+        // ListView<Recipe> recipessListView = lookup("#mainListView").query();
+        // recipessListView.get
+        Predicate<ListViewCell> listCell = cell -> cell.lookup(".label") != null;
+        clickOn(listViewCell(listCell, 1));;
+
+        // checkRecipesListViewItems(recipe1, recipe2, recipe3);
+    }
+
+    private Node waitForNode(Predicate<Node> nodeTest, int num) {
+        WaitForAsyncUtils.waitForFxEvents();
+        Node[] nodes = new Node[1];
+        try {
+            WaitForAsyncUtils.waitFor(200, TimeUnit.MILLISECONDS, () -> {
+                while (true) {
+                    if ((nodes[0] = findNode(nodeTest, num)) != null) {
+                        return true;
+                    }
+                    Thread.sleep(100);
+                }
+            });
+        } catch (TimeoutException e) {
+            fail("No appropriate node available");
+        }
+        return nodes[0];
+    }
+
+    private Node findNode(Predicate<Node> nodeTest, int num) {
+        int count = 0;
+        for (Node node : lookup(nodeTest).queryAll()) {
+            if (nodeTest.test(node) && count++ == num) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    private ListViewCell listViewCell(Predicate<ListViewCell> test, int num) {
+        return (ListViewCell) waitForNode(node -> node instanceof ListViewCell listCell && test.test(listCell), num);
     }
 
 }
