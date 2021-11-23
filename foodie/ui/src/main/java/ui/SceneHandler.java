6fe0d842a35796005d7d39a8f7a93f5081cc5f18
Nicoline Mork <nicolimo@stud.ntnu>
2021-11-23 09:43:44 +0100

    feat: class to handle scenechange

---
+++ foodie/ui/src/main/java/ui/SceneHandler.java
@@ -0,0 +1,47 @@
+package ui;
+
+import core.Recipe;
+import javafx.stage.Stage;
+import ui.utils.CookbookAccess;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SceneHandler {
+
+  
+  
+
+  private static final String MAIN_FXML = "Main.fxml";
+  private static final String VIEW_RECIPE_FXML = "ViewRecipe.fxml";
+  private static final String NEW_RECIPE_FXML = "NewRecipe.fxml";
+  private static final String LISTVIEW_FXML = "ListView.fxml";
+
+  private static Map<SceneName, FxmlModel> scenes = new HashMap<>();
+
+  /** @return a Map of the {@link FxmlInfo} by {@link SceneName} */
+  public static Map<SceneName, FxmlModel> getScenes() {
+    return new HashMap<SceneName, FxmlModel> (scenes);
+  }
+
+  public static void initialize(Stage stage){
+    scenes.put(SceneName.MAIN, new FxmlModel(MAIN_FXML, SceneName.MAIN, stage));
+    scenes.put(SceneName.VIEWRECIPE, new FxmlModel(VIEW_RECIPE_FXML, SceneName.VIEWRECIPE, stage));
+    scenes.put(SceneName.NEWRECIPE, new FxmlModel(NEW_RECIPE_FXML, SceneName.NEWRECIPE, stage));
+    scenes.put(SceneName.LISTVIEW, new FxmlModel(LISTVIEW_FXML, SceneName.LISTVIEW, stage));
+  }
+  /**
+   * Update the scene Map with new FxmlInfo
+   * 
+   * @param name the {@link SceneName} that is the key to update
+   * @param info the {@link FxmlInfo} that is the data to update
+   */
+  public static void updateScenes(SceneName name, FxmlModel info) {
+    scenes.put(name, info);
+  }
+
+ 
+
+
+
+  
+}
